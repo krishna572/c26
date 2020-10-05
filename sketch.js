0baacf23d4dffb303272cd72ccd34292f42f06d0
krishna6174229@gmail.com <kk6174229@gmail.com>
2020-10-05 15:26:21 -0700

    added a platform

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    platform = new Ground(150,305,300,170);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -26,6 +27,7 @@ function setup(){
     box3 = new Box(700,240,70,70);
     box4 = new Box(920,240,70,70);
     pig3 = new Pig(810, 220);
+
 
     log3 =  new Log(810,180,300, PI/2);
 
@@ -59,4 +61,6 @@ function draw(){
     log5.display();
 
     bird.display();
+
+    platform.display();
 }
